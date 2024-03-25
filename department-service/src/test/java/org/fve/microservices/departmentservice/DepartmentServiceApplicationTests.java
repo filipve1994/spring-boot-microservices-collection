38383bd022481c2c712fbcaf88523709ca758157
Filip Vanden Eynde <47209284+filipve1994@users.noreply.github.com>
2024-03-25 11:29:05 +0000

    add department-service

---
+++ department-service/src/test/java/org/fve/microservices/departmentservice/DepartmentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.fve.microservices.departmentservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DepartmentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
