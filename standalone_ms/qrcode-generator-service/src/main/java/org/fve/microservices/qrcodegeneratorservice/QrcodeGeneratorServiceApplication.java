2916256e23f0e809e868276cd4d5ed48691b3059
Filip Vanden Eynde <47209284+filipve1994@users.noreply.github.com>
2024-03-25 12:29:25 +0000

    Add files via upload

---
+++ standalone_ms/qrcode-generator-service/src/main/java/org/fve/microservices/qrcodegeneratorservice/QrcodeGeneratorServiceApplication.java
@@ -0,0 +1,13 @@
+package org.fve.microservices.qrcodegeneratorservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QrcodeGeneratorServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QrcodeGeneratorServiceApplication.class, args);
+	}
+
+}
---
+++ standalone_ms/qrcode-generator-service/src/test/java/org/fve/microservices/qrcodegeneratorservice/QrcodeGeneratorServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.fve.microservices.qrcodegeneratorservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QrcodeGeneratorServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
