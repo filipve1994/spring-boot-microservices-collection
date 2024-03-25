32b6b604bb6622df4732fdb441d38f216d5977cc
Filip Vanden Eynde <47209284+filipve1994@users.noreply.github.com>
2024-03-25 12:08:30 +0000

    add mail-service

---
+++ standalone_ms/mail-service/src/test/java/org/fve/microservices/mailservice/MailServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.fve.microservices.mailservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MailServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
