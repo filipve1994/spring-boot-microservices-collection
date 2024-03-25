959c1dae60ec1328b88d630b9f2ec068f457172c
Filip Vanden Eynde <47209284+filipve1994@users.noreply.github.com>
2024-03-25 11:30:46 +0000

    add organization-service

---
+++ organization-service/src/test/java/org/fve/microservices/organizationservice/OrganizationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.fve.microservices.organizationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrganizationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
